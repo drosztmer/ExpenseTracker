2cd2c74806fc96039eac6f2cf632a5bc6143fcb5
Drosztmér István <drosztmer.istvan@gmail.com>
2021-06-11 11:46:58 +0200

    Add model

---
+++ app/src/main/java/com/drosztmer/expensetracker/data/model/Expense.kt
@@ -0,0 +1,16 @@
+package com.drosztmer.expensetracker.data.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "expense_table")
+@Parcelize
+data class Expense(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    var timeCreated: String,
+    var title: String,
+    var amount: Int
+): Parcelable
