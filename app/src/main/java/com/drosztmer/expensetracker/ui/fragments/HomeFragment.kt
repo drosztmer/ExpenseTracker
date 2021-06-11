9897daf69a5eb48531828635653c7319364d48e5
Drosztmér István <drosztmer.istvan@gmail.com>
2021-06-11 11:40:42 +0200

    Implement navigating to CreateExpenseFragment

--- app/src/main/java/com/drosztmer/expensetracker/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/drosztmer/expensetracker/ui/fragments/HomeFragment.kt
@@ -2,18 +2,29 @@ package com.drosztmer.expensetracker.ui.fragments
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.drosztmer.expensetracker.R
+import com.drosztmer.expensetracker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_createExpenseFragment)
+        }
         // Inflate the layout for this fragment
         setHasOptionsMenu(true)
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
