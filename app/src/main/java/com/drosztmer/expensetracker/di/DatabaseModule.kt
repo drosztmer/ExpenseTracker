f16836943e8854abedc44da9655a5c558c679a2f
Drosztmér István <drosztmer.istvan@gmail.com>
2021-06-11 12:00:48 +0200

    Use di

---
+++ app/src/main/java/com/drosztmer/expensetracker/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.drosztmer.expensetracker.di
+
+import android.content.Context
+import androidx.room.Room
+import com.drosztmer.expensetracker.data.database.ExpenseDatabase
+import com.drosztmer.expensetracker.util.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) =
+        Room.databaseBuilder(context, ExpenseDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: ExpenseDatabase) = database.expenseDao()
+
+}
