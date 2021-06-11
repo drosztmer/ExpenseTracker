e8385b2c826a792793b811af67cca1328aac9ca5
Drosztmér István <drosztmer.istvan@gmail.com>
2021-06-11 10:41:15 +0200

    Add menu, floating action button to HomeFragment

--- app/src/main/java/com/drosztmer/expensetracker/ui/fragments/HomeFragment.kt
+++ app/src/main/java/com/drosztmer/expensetracker/ui/fragments/HomeFragment.kt
@@ -1,10 +1,8 @@
 package com.drosztmer.expensetracker.ui.fragments
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.drosztmer.expensetracker.R
 
 class HomeFragment : Fragment() {
@@ -14,7 +12,12 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.home_fragment_menu, menu)
     }
 
 }
